b0ac877c1047bcb803ebfdf3c11192d44af2fdcc
badfilemagic <badfilemagic@users.noreply.github.com>
2017-04-09 22:00:22 -0400

    Create connection-mgmt.sh
    Append this to your ~/.bashrc for working with IPsec VPNs. Provides the ability to randomly select a VPN configuration from /etc/ipsec.conf so that you can cycle between tunnel peers provided by a VPN service such as NordVPN or similar.

---
+++ connection-mgmt.sh
@@ -0,0 +1,42 @@
+# (C) 2017, W. Dean Freeman, CISSP CSSLP GCIH
+# v0.1 
+# Append this to your ~/.bashrc
+# Requires that you install rand (sudo apt-get install rand)
+# StrongSwan or similar should be an obvious dependency...
+# 
+# Connect/disconnect to random IKEv2/IPsec endpoint from list configured in /etc/ipsec.conf
+# Cycle vpn connections
+# Disable/re-enable ivp6 to avoid leakage
+
+IPSEC=/usr/sbin/ipsec
+RAND=/usr/bin/rand
+export VPN=""
+vpn_up()
+{
+        random_vpn
+        sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+        sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+        sudo $IPSEC up $VPN
+}
+vpn_down()
+{
+        sudo $IPSEC down $VPN
+        sudo sysctl -w net.ipv6.conf.all.disable_ipv6=0
+        sudo sysctl -w net.ipv6.conf.default.disable_ipv6=0
+}
+vpn_cycle()
+{
+        vpn_down
+        vpn_up
+}
+list_vpns()
+{
+        grep ^conn /etc/ipsec.conf | awk '{print $2}'
+}
+random_vpn()
+{
+        VPNS=(`list_vpns`)
+        n=`list_vpns | wc -l`
+        i=`$RAND -M $n`
+        VPN=${VPNS[$i]}
+}
